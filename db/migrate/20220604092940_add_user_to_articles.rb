a43a7e8dffb166a5db259ffe723cefc412240292
sergioalejandrogs <sergioalejandrogs@gmail.com>
2022-06-06 18:45:33 -0500

    Fixed some bugs with signing up and adding comments

--- db/migrate/20220604092940_add_user_to_articles.rb
+++ db/migrate/20220604092940_add_user_to_articles.rb
@@ -1,7 +1,7 @@
 class AddUserToArticles < ActiveRecord::Migration[7.0]
   def change
     unless column_exists?(:articles, :user_id)  
-      add_reference :articles, :user, null: false, foreign_key: true
+      add_reference :articles, :user, null: true, foreign_key: true
     end
   end
 end
