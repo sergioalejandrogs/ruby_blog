6a0316ced79a51c324ed980f1fa88228dfa12370
sergioalejandrogs <sergioalejandrogs@gmail.com>
2022-06-06 18:45:33 -0500

    Fixed some bugs with signing up and adding comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,18 +5,24 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
-    @comment.destroy
-    redirect_to article_path(@article), status: 303
+    if current_user.id == @comment.user_id  
+      @comment.destroy
+      redirect_to article_path(@article), status: 303
+    else
+      redirect_to article_path(@article)
+    end
   end
 
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body, :status)
+      params.require(:comment).permit(:commenter, :body, :status, :article_id, :user_id)
     end
 end
